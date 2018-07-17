81ed171fe07b3e852e3bed817a4050479da4790c
itReverie <brenda@itreverie.com>
2018-07-17 19:08:50 -0400

    Adding unit testing for the web socket

--- src/webSocket/index.js
+++ src/webSocket/index.js
@@ -1,9 +1,11 @@
-import io from 'socket.io-client';
 import {webSocket} from '../config/';
+import io from 'socket.io-client';
 
 const socket = io(webSocket.server);
+let messages = [];
 
 const init = (store) => {
+  messages=[];
   // add listeners to socket messages so we can re-dispatch them as actions
   Object.keys(webSocket.messages)
     .forEach(type => 
@@ -12,6 +14,14 @@ const init = (store) => {
 }
 
 const emit = (type, favoriteGif) => {
-              socket.emit(type, favoriteGif)}
+              
+              messages.push(type,favoriteGif);
+              socket.emit(type, favoriteGif)
+            }
 
-export {init,emit}
+
+//This function is a helper for testing purposes
+const getMessages = () => {
+                return messages;};
+
+export {init,emit, getMessages}
--- src/webSocket/webSocket.test.js
+++ src/webSocket/webSocket.test.js
@@ -1,19 +1,89 @@
-import React from 'react';
-import WebSocket from './';
-import { SocketIO as socketIO, Server } from 'mock-socket';
+import { init as websocketInit, emit , getMessages  }  from './';
 import {webSocket} from '../config/';
+import { SocketIO as socketIO, Server } from 'mock-socket';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import * as favoriteActions from '../actions/favoriteActions';
+import * as gifActions from '../actions/gifActions';
+import * as types from "../actions/actionTypes";
+import gif from '../mocks/gif';
 
+
+//STORE
+const middleware = [thunk.withExtraArgument({ emit })];
+const mockStore = configureMockStore(middleware);
+let store;
+
+//SERVER
 const mockServer = new Server( webSocket.server);
+window.io = socketIO;
 
 describe('setting correctly the messages and emit to use in the socket client', ()=>{
 
-    // beforeEach(() => {
-    //   onSearch=jest.fn();
-    // });
+    beforeEach(() => {
+        store = mockStore();
+        websocketInit( store );
+      });
 
-    it('reading messages for the socket client', () => {
-      
-      
-       //expect(onSearch).toHaveBeenCalledTimes(1);
+    it('emitting ADD_FAVORITE_GIF message for the socket client', () => {
+        expect.assertions(2);
+
+        //Initializing 
+        const favoriteGif={ gif: {[gif.id]:gif} ,isFavorite:true};
+        const message= webSocket.messages.ADD_FAVORITE_GIF;
+
+        //Expected results
+        const expectedEmitAdd = [message,favoriteGif];
+        const expectedActionAdd = { type: types.ADD_FAVORITE_GIF, favoriteGif };
+
+        //Testing emit SEND by mocking an action dispatch
+        store.dispatch(favoriteActions.addFavorite(favoriteGif));
+        expect( getMessages() ).toEqual( expectedEmitAdd );
+
+        //Testing emit RECEIVED by mocking a server emit
+        mockServer.emit(message,favoriteGif);
+        expect( store.getActions()[0] ).toEqual(expectedActionAdd);
     });
+
+
+    it('emitting REMOVE_FAVORITE_GIF message for the socket client', () => {
+        expect.assertions(2);
+
+        //Initializing 
+        const favoriteGif={ gif: {[gif.id]:gif} ,isFavorite:false};
+        const message= webSocket.messages.REMOVE_FAVORITE_GIF;
+
+        //Expected results
+        const expectedEmitRemove = [message,favoriteGif];
+        const expectedActionRemove = { type: types.REMOVE_FAVORITE_GIF, favoriteGif };
+
+        //Testing emit SEND by mocking an action dispatch
+        store.dispatch(favoriteActions.addFavorite(favoriteGif));
+        expect( getMessages() ).toEqual( expectedEmitRemove );
+
+        //Testing emit RECEIVED by mocking a server emit
+        mockServer.emit(message,favoriteGif);
+        expect( store.getActions()[0] ).toEqual(expectedActionRemove);
+    });
+
+    it('emitting SET_FAVORITE_GIF message for the socket client', () => {
+        expect.assertions(2);
+
+        //Initializing 
+        const favoriteGif={ gif: {[gif.id]:gif} ,isFavorite:false};
+        const message= webSocket.messages.SET_FAVORITE_GIF;
+
+        //Expected results
+        const expectedEmitRemove = [message,favoriteGif];
+        const expectedActionRemove = { type: types.SET_FAVORITE_GIF, favoriteGif };
+
+        //Testing emit SEND by mocking an action dispatch
+        store.dispatch(gifActions.setFavorite(favoriteGif));
+        expect( getMessages() ).toEqual( expectedEmitRemove );
+
+        //Testing emit RECEIVED by mocking a server emit
+        mockServer.emit(message,favoriteGif);
+        expect( store.getActions()[0] ).toEqual(expectedActionRemove);
+    });
+
 });
