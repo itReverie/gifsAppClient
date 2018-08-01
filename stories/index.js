5953addc89b46bcb0235886d7915bd4cc573cabe
itReverie <brenda@itreverie.com>
2018-07-31 23:36:49 -0400

    Adding knobs

--- stories/index.js
+++ stories/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
+import { withKnobs, text, boolean, number } from '@storybook/addon-knobs';
 import SearchInput from '../src/components/searchInput';
 import Star from '../src/components/star';
 
-storiesOf('Components', module)
-  .add('Search', () => (
+const stories = storiesOf('Components Knobs', module);
+
+// Add the `withKnobs` decorator to add knobs support to your stories.
+// You can also configure `withKnobs` as a global decorator.
+stories.addDecorator(withKnobs);
+
+
+stories.add('Search', () => (
     <SearchInput onClick={action('clicked')} />
-  ))
-  .add('Star', () => (
-    <Star onClick={action('clicked')} />
+  ));
+
+  stories.add('Star', () => (
+    <Star onClick={action('clicked')} active={boolean('Disabled', false)}  />
   ));
